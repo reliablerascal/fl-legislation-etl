90ac0e267b9a63c18eab4e2c8960a5d648a2689d
apantazi <andrew.pantazi@gmail.com>
2024-08-01 17:11:50 -0400

    Updated to automate without running against cache limit for API.
    Now it saves the dataset list to an RDS object. Then each time this runs, it reads in the previous dataset_list, compares the dataset_hash in that list to a fresh dataset_list and only reads in a dataset if it has a new dataset_hash.

--- scripts/01_request_api_legiscan.R
+++ scripts/01_request_api_legiscan.R
@@ -11,9 +11,40 @@ library(legiscanrr) # Interface with the LegiScan API for accessing legislative 
 #requires user to enter their api key
 legiscan_api_key(set_new=TRUE)
 
+# Define the data directory path
+dir_path <- "../data-raw/"
+
+# Check if the directory exists
+if (!dir.exists(dir_path)) {
+  # If it doesn't exist, create the directory
+  dir.create(dir_path, recursive = TRUE)
+}
+
+# File to store the list of existing datasets
+existing_datasets_file <- "../data-raw/existing_datasets.rds"
+
+if (file.exists(existing_datasets_file)) {
+  existing_datasets <- readRDS(existing_datasets_file)
+} else {
+  existing_datasets <- data.frame(dataset_hash = character())
+}
+
 #get list of datasets
 list_datasets_fl <- legiscanrr::get_dataset_list("fl") 
 
-## download every session, ??but warning about API limits. #### 
-# put all datasets in data-raw
-purrr::walk(list_datasets_fl, get_dataset, save_to_dir = "../data-raw/legiscan") 
+new_hashes <- sapply(list_datasets_fl, function(x) x$dataset_hash)
+existing_hashes <- sapply(existing_datasets, function(x) x$dataset_hash)
+
+datasets_to_download <- list_datasets_fl[!new_hashes %in% existing_hashes]
+
+if (length(datasets_to_download) > 0) {
+  # Download new datasets
+  purrr::walk(datasets_to_download, get_dataset, save_to_dir = "../data-raw/legiscan")
+  
+  cat("Downloaded", length(datasets_to_download), "new or updated datasets.\n")
+} else {
+  cat("No new or updated datasets found.\n")
+}
+
+# Update the existing datasets file
+saveRDS(object = list_datasets_fl, existing_datasets_file)
